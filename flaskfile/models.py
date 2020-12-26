c3f58ebacdaf21b8ce1fff5805c2acdce16ef9b2
William <williamallen12364@gmail.com>
2020-12-26 13:47:51 +0800

    revert to before adding reset password with email

--- flaskfile/models.py
+++ flaskfile/models.py
@@ -16,18 +16,6 @@ class User(db.Model, UserMixin):
         db.String(20), default='default.jpg', nullable=False)
     password = db.Column(db.String(60), nullable=False)
 
-    def get_reset_token(self, expires_sec=1800):
-        s = Serializer(app.config['SECRET_KEY'], expires_sec)
-        return s.dumps({'user_id': self.id}).decode('utf-8')
-
-    @staticmethod
-    def verify_reset_token(token):
-        s = Serializer(app.config['SECRET_KEY'])
-        try:
-            user_id = s.loads(token)['user_id']
-        except:
-            return None
-        return User.query.get(user_id)
-
+    
     def __repr__(self):
         return f"User('{self.username}', '{self.email}', '{self.image_file}')"
